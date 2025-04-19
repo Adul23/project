95fecf1ce392d2f71d56604e1f500e5fb5daa2df
Adul23 <adilgg672@gmail.com>
2025-04-19 15:28:40 +0500

    backend changes

---
+++ backend/mmoney_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MmoneyTrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mmoney_tracker'
--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/adil/swift/project/backend/venv
+set -gx VIRTUAL_ENV /home/adil/swift/project/project/backend/money_tracker/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
