8f5547c694208f4a3955ced95b81257dc2efe1cf
Дилмухаммед <157337455+Dinuharu@users.noreply.github.com>
2025-04-23 10:57:11 +0500

    Delete backend directory

--- backend/mmoney_tracker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MmoneyTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mmoney_tracker'
--- backend/mmoney_tracker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BudgetListView, listExpenses, listExpensesbyBudget
-
-urlpatterns = [
-    path('api/budgets/', BudgetListView.as_view(), name='budget-list'),
-    path('api/expenses/', listExpenses, name='budget-expenses'),
-    path('api/budgets/<int:budget_id>/expenses/', listExpensesbyBudget, name='budget-expenses'),
-
-]
